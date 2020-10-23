b60604ffc181fdd1483d6106089b1d486fb4d0be
shunici <32427612+shunici@users.noreply.github.com>
2020-10-23 23:56:47 +0700

    Create App.vue

---
+++ vuex/tambah_data/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <div class="row py-4">
+      <agenda-input/>
+      <agenda-show/>
+    </div>
+    
+
+  </div>
+</template>
+
+<script>
+import AgendaInput from './components/AgendaInput.vue'
+import AgendaShow from './components/AgendaShow.vue'
+
+export default {
+  name: 'App',
+  components: {
+   AgendaInput,
+   AgendaShow
+  },
+ 
+  
+}
+</script>
+
+<style >
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
