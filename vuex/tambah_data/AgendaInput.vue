2b6204f04195d450e881dd68665bca60e3a1ec66
shunici <32427612+shunici@users.noreply.github.com>
2020-10-23 23:57:52 +0700

    Add files via upload

---
+++ vuex/tambah_data/AgendaInput.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>   
+   <div class="card">
+        <div class="card-header">
+           Ini adalah agenda input
+        </div>
+        <div class="card-body">
+            <div class="form-group">
+                <label for="">Nama</label>
+                <input type="text"
+                    class="form-control" name="" v-model="profil.nama" id="" aria-describedby="helpId" placeholder="">
+                <small id="helpId" class="form-text text-muted">{{profil.nama}}</small>
+            </div>
+             <div class="form-group">
+                <label for="">Klub</label>
+                <input type="text"
+                    class="form-control" name="" id="" v-model="profil.klub"  aria-describedby="helpId" placeholder="">
+                <small id="helpId" class="form-text text-muted">{{profil.klub}}</small>
+            </div>
+             <button type="button" class="btn btn-primary"  :disabled="isLoading"  @click='simpan'>{{ isLoading ? 'Loading...':'Tambahkan' }}</button>
+        </div>
+        <div class="card-footer text-muted">
+            Footer
+        </div>
+</div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: 'AgendaInput',
+        data (){
+            return {
+              profil : {
+                    nama : '',
+                    klub : '',
+              }
+            }
+        },
+        methods : {
+            simpan(){
+            //dispatch menyambung dengan action di store.js
+              this.$store.dispatch('simpan_profil', this.profil)
+              //line ini berfungsi ketika tombol diklik maka kosong data di inputan
+              this.profil = {
+                  nama : '',
+                  klub : ''
+              }
+            }
+        },
+        computed: {
+            isLoading() {
+                return this.$store.state.isLoading
+            }
+        },
+       
+    }
+</script>
---
+++ vuex/tambah_data/AgendaShow.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>   
+   <div class="card">
+        <div class="card-header">
+           Ini adalah agenda show
+        </div>
+        <div class="card-body">
+           <ul><li v-for="item in profil" :key="item.id">dia adalah {{item.nama}} yang berasal dari klub {{item.klub}}</li></ul>
+        </div>
+        <div class="card-footer text-muted">
+            Footer
+        </div>
+</div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: 'AgendaShow',
+        computed : {
+            profil(){
+                return this.$store.state.profil
+            }
+        }
+    }
+</script>
