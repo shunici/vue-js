0b737ec7153d8e8d5d2e3553ba57e2cf90912fba
shunici <32427612+shunici@users.noreply.github.com>
2021-03-04 20:54:09 +0700

    app.vue

---
+++ template_awal/vuex_router_vue_bootstrap/resource/js/app.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+     <headerComponent></headerComponent>         
+   <div class="container">
+          <router-view></router-view>
+   </div>
+   <br> <br> <br>
+     <footer-component></footer-component>         
+    </div>
+</template>
+
+<script>
+import headerComponent from './components/headerComponent.vue';
+import footerComponent from './components/footerComponent.vue';
+
+    export default {
+       components : {
+           headerComponent,footerComponent
+       }
+    }
+</script>
